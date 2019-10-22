e76991a802c7979aae007242e672387ba2fd3c1c
zien <sheenazien08@gmail.com>
2019-10-22 11:10:21 +0700

    [PORTFOLIO] EXPORT SQL

--- personal_blog.sql
+++ personal_blog.sql
@@ -35,7 +35,7 @@ CREATE TABLE `blogs` (
   `user_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_index` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,8 +44,40 @@ CREATE TABLE `blogs` (
 
 LOCK TABLES `blogs` WRITE;
 /*!40000 ALTER TABLE `blogs` DISABLE KEYS */;
-INSERT INTO `blogs` VALUES (1,'Jangan Malu Jadi Karyawan','jangan-malu-jadi-karyawan',' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, nihil voluptates, facere rem velit eius labore, voluptatum et consequatur reprehenderit excepturi laborum placeat voluptate. Magni rerum nam molestias, totam eveniet.','2019-10-21',1,50,' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, nihil ','meta, meta, meta, meta',1),(2,'Mudahnya jadi pekerja yang banyak bersyuku','Mudahnya-jadi-pekerja-yang-banyak-bersyuku','\r\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2019-10-22',0,0,'Lorem ipsum dolor sit amet, consectetur ','html, css, javascript',1),(8,'Hakikat Wani','Hakikat-Wani','lorem','2019-10-22',0,0,'lorem','lorem',1);
+INSERT INTO `blogs` VALUES (1,'Jangan Malu Jadi Karyawan','jangan-malu-jadi-karyawan',' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, nihil voluptates, facere rem velit eius labore, voluptatum et consequatur reprehenderit excepturi laborum placeat voluptate. Magni rerum nam molestias, totam eveniet.','2019-10-21',1,50,' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, nihil ','meta, meta, meta, meta',1),(9,'Kenapa Sih Harus Menimplementasikan Sistem Management ke Bisnis?','Kenapa-Sih-Harus-Menimplementasikan-Sistem-Management-ke-Bisnis?',' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, nihil voluptates, facere rem velit eius labore, voluptatum et consequatur reprehenderit excepturi laborum placeat voluptate. Magni rerum nam molestias, totam eveniet.','2019-10-22',0,0,'meta','meta',1);
 /*!40000 ALTER TABLE `blogs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portfolios`
+--
+
+DROP TABLE IF EXISTS `portfolios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portfolios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `date` date NOT NULL,
+  `meta_description` varchar(255) NOT NULL,
+  `meta_keyword` varchar(255) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portfolios`
+--
+
+LOCK TABLES `portfolios` WRITE;
+/*!40000 ALTER TABLE `portfolios` DISABLE KEYS */;
+INSERT INTO `portfolios` VALUES (2,'asdasdaa','asdasdaa','asda','sada',0,'2019-10-22','asd','sadad',1);
+/*!40000 ALTER TABLE `portfolios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -84,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-22 10:31:56
+-- Dump completed on 2019-10-22 11:10:12
